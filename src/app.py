28bab568593c8111010a1dd69b7398382e58df3c
iiihenrymorrin <henry.morrin@iii.com>
2018-12-18 15:07:04 +0000

    Started to build app and updated requirements file

--- src/app.py
+++ src/app.py
@@ -1,1 +1,11 @@
-pass
+__author__="Henry Morrin"
+
+import requests
+from bs4 import BeautifulSoup
+
+request=requests.get("https://cloud.servecentric.com/")
+content=request.content
+print(type(content))
+soup=BeautifulSoup(content,"html.parser")
+
+#Need to retrieve data from html returned
